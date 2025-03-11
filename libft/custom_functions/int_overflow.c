1ed2d86c33f4b14dcb307e9831bf212cf4fffafb
Welepy-dev <mwsilva.contato@gmail.com>
2025-03-11 21:05:17 +0000

    the two functions that were added and a small change to strcmp

--- libft/custom_functions/int_overflow.c
+++ libft/custom_functions/int_overflow.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   int_overflow.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: welepy <welepy@student.42.fr>              +#+  +:+       +#+        */
+/*   By: welepy </var/spool/mail/welepy>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/14 11:45:00 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/03/11 20:22:53 by welepy           ###   ########.fr       */
+/*   Created: 2025/03/11 13:59:00 by welepy            #+#    #+#             */
+/*   Updated: 2025/03/11 14:01:09 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+bool  int_overflow(long long int n)
 {
-	unsigned int	i;
-
-	i = 0;
-	if (s1 || s2)
-		return (1);
-	while (s1[i] != '\0' && s1[i] == s2[i])
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (n < LLONG_MIN || n > LLONG_MAX);
 }
