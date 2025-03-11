57042e6baeadf5c38d45e012cc1260c01c423173
Welepy-dev <mwsilva.contato@gmail.com>
2025-03-11 21:06:16 +0000

    finished exit

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: welepy <welepy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 15:49:03 by mchingi           #+#    #+#             */
-/*   Updated: 2025/03/10 21:07:11 by welepy           ###   ########.fr       */
+/*   Updated: 2025/03/11 14:21:20 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static bool	all_nums(char *string)
 	int	i;
 
 	i = -1;
-	if (string[0] == '-')
+	if (string[0] == '-' || string[0] == '+')
 	{
 		i = 0;
 		if (string[1] && !ft_isdigit(string[1]))
@@ -68,26 +68,28 @@ static int	modulo(long long int exit_status)
 
 void	ft_exit(t_shell *shell, t_token *token)
 {
-	char	*trimmed;
-	if(token->next)
+	char	*arr[2];
+
+	if (token->next)
 	{
-		trimmed = ft_strtrim(token->next->value, " \n\t");
-		if (trimmed)
-			shell->exit_status = modulo(ft_atoi(trimmed));
-		if (!all_nums(trimmed))
+		arr[0] = ft_strtrim(token->next->value, " \n\t");
+		errno = 0;
+		if (arr[0])
+			shell->exit_status = modulo((strtoll(arr[0], &arr[1], 10)));
+		if (!all_nums(arr[0]) || all_spaces(token->next->value) || *arr[1]
+			|| errno == ERANGE || int_overflow(strtoll(arr[0], &arr[1], 10)))
 		{
 			shell->exit_status = 2;
-			ft_fprintf(2, "exit: %s numeric argument required\n",
-				 (trimmed));
+			ft_fprintf(2, "exit: %s numeric argument required\n", (arr[0]));
 		}
 		else if (token->next->next)
 		{
 			shell->exit_status = 1;
 			ft_fprintf(2, "exit: too many arguments\n");
-			ft_free(&trimmed);
+			ft_free(&arr[0]);
 			return ;
 		}
-		ft_free(&trimmed);
+		ft_free(&arr[0]);
 	}
 	clean_or_exit(shell);
 	exit(shell->exit_status);
