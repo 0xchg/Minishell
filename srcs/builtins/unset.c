a922ae50ba5e2df500cc1c9ba5c769499a089a12
Welepy-dev <mwsilva.contato@gmail.com>
2025-03-15 21:35:34 +0000

    new unset logic and option validation

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,46 +6,51 @@
 /*   By: welepy <welepy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 15:49:17 by mchingi           #+#    #+#             */
-/*   Updated: 2025/03/12 16:14:43 by welepy           ###   ########.fr       */
+/*   Updated: 2025/03/15 21:35:18 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minihell.h"
 
-void	remove_env(t_env **head, const char *name)
+void remove_env(t_env **env, char *name)
 {
-	t_env	*current;
-	t_env	*previous;
+	t_env *current = *env;
+	t_env *prev = NULL;
+	char  **tok;
 
-	current = *head;
-	previous = NULL;
+	tok = ft_split(name, '=');
 	while (current)
 	{
-		if (strcmp(current->name, name) == 0)
+		if (!ft_strcmp(current->name, tok[0]))
 		{
-			if (previous)
-				previous->next = current->next;
+			if (prev)
+				prev->next = current->next;
 			else
-				*head = current->next;
-			if (current->name)
-				free(current->name);
-			if (current->value)
-				free(current->value);
+				*env = current->next;
+			free(current->name);
+			free(current->value);
 			free(current);
-			return ;
+			free_matrix(tok);
+			return;
 		}
-		previous = current;
+		prev = current;
 		current = current->next;
 	}
+	free_matrix(tok);
 }
 
 void	ft_unset(t_env *env, t_token *token, t_shell *shell, bool flag)
 {
 	t_token	*temp;
-
 	temp = token;
 	if (!flag)
-		temp = temp->next;
+		temp = token->next;
+	if (token->next && token->next->type == OPTION)
+	{
+		shell->exit_status = 1;
+		ft_fprintf(2, "unset: this version doesn't support options");
+		return ;
+	}
 	while (temp && temp->type == ARGUMENT)
 	{
 		remove_env(&env, temp->value);
