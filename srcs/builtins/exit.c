7517a5f2a76d04b801a1b8c958f037350e65890d
Welepy-dev <mwsilva.contato@gmail.com>
2025-03-20 11:38:48 +0100

    exit tweaks

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: welepy <welepy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 15:49:03 by mchingi           #+#    #+#             */
-/*   Updated: 2025/03/11 14:21:20 by welepy           ###   ########.fr       */
+/*   Updated: 2025/03/12 22:41:17 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,6 +76,7 @@ void	ft_exit(t_shell *shell, t_token *token)
 		errno = 0;
 		if (arr[0])
 			shell->exit_status = modulo((strtoll(arr[0], &arr[1], 10)));
+		printf("exit\n");
 		if (!all_nums(arr[0]) || all_spaces(token->next->value) || *arr[1]
 			|| errno == ERANGE || int_overflow(strtoll(arr[0], &arr[1], 10)))
 		{
@@ -85,9 +86,8 @@ void	ft_exit(t_shell *shell, t_token *token)
 		else if (token->next->next)
 		{
 			shell->exit_status = 1;
-			ft_fprintf(2, "exit: too many arguments\n");
 			ft_free(&arr[0]);
-			return ;
+			return (ft_fprintf(2, "exit: too many arguments\n"));
 		}
 		ft_free(&arr[0]);
 	}
